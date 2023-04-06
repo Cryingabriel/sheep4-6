c8a1a058452d3e81af4a59cd65acb55bfddc03d5
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2023-04-06 14:38:00 -0600

    llllllllllllllllllllllllllllllll

--- python top down game/python_top_down_game.py
+++ python top down game/python_top_down_game.py
@@ -30,14 +30,17 @@ DOWN = 3
 def sheepMove(position):
     if timer % 50 == 0: #only change direction every 50 game loops
         position[2] = random.randrange(0, 4) #set random direction
-    if position[2] == LEFT and position[2] > 0:
-        position[0]-=2 #move left
-    elif position[2] == RIGHT and position[2] < 800:
-        position[0] += 2 #move right
-    elif position[2] == UP: 
-        position[1] +=2 #move up
-    else:
-        position[0] -=2 #move down
+
+    if position[2] == LEFT and position[0] >= 0:
+        position[0]-=8 #move left
+
+    elif position[2] == RIGHT and position[0]+80 <= 800:
+        position[0] += 8 #move right
+
+    elif position[2] == UP and position[1] >= 0:
+        position[1] -=8 #move up
+    elif position[1]+50 <= 800:
+        position[1] +=8 #move down
     return position
 
 def collision(PlayerX, PlayerY, sheepInfo):
@@ -46,15 +49,19 @@ def collision(PlayerX, PlayerY, sheepInfo):
            if PlayerY+40 >sheepInfo[1]:
                if PlayerY < sheepInfo[1]+40:
                    if sheepInfo[3] == False: #only catch uncaught sheeps!
-                    sheepInfo[3] = True #catch da sheepies!
-                    global score #make it so this function can change this value
-                    score +=1
+                        sheepInfo[3] = True #catch da sheepies!
+                        global score #make it so this function can change this value
+                        score +=1
 
 
 
 #create sheep!
 #numbers in list represent xpos, ypos, direction moving, and whether it's been caught or not!
 sheep1 = [200, 400, RIGHT, False]
+sheep2 = [200, 400, LEFT, False]
+sheep3 = [200, 400, UP, False]
+sheep4 = [200, 400, DOWN, False]
+sheep5 = [200, 300, LEFT, False]
 #make more sheeps here!
 
 
@@ -66,7 +73,7 @@ vx = 0 #x velocity (left/right speed) of player
 vy = 0 #y velocity (up/down speed) of player
 keys = [False, False, False, False] #this list holds whether each key has been pressed
 
-while score<1: #GAME LOOP############################################################
+while score<5: #GAME LOOP############################################################
     clock.tick(60) #FPS
     timer+=1
     
@@ -103,15 +110,25 @@ while score<1: #GAME LOOP#######################################################
     
     #player movement!--------
     if keys[LEFT] == True:
-        vx = -3
+        vx = -8
     elif keys[RIGHT] == True:
-        vx = 3
+        vx = 8
     else:
         vx = 0
+    if keys[UP] == True:
+        vy = -8
+    elif keys[DOWN] == True:
+        vy = 8
+    else:
+        vy = 0
 
 
     #player/sheep collision!
     collision(xpos, ypos, sheep1)
+    collision(xpos, ypos, sheep2)
+    collision(xpos, ypos, sheep3)
+    collision(xpos, ypos, sheep4)
+    collision(xpos, ypos, sheep5)
 
     #update player position
     xpos+=vx 
@@ -119,7 +136,10 @@ while score<1: #GAME LOOP#######################################################
     
     #update sheep position
     sheep1 = sheepMove(sheep1)
-  
+    sheep2 = sheepMove(sheep2)
+    sheep3 = sheepMove(sheep3)
+    sheep4 = sheepMove(sheep4)
+    sheep5 = sheepMove(sheep5)
     # RENDER
     # Section--------------------------------------------------------------------------------
             
@@ -133,6 +153,25 @@ while score<1: #GAME LOOP#######################################################
         #pygame.draw.rect(screen, (250, 100, 100), (sheep1[0], sheep1[1], 40, 40)) #use this if you don't have a jpg
         screen.blit(SheepPic, (sheep1[0], sheep1[1]))
     
+    if sheep2[3] == False: #don't draw them if they're already caught!
+        #pygame.draw.rect(screen, (250, 100, 100), (sheep1[0], sheep1[1], 40, 40)) #use this if you don't have a jpg
+        screen.blit(SheepPic, (sheep2[0], sheep2[1]))
+    
+    if sheep3[3] == False: #don't draw them if they're already caught!
+        #pygame.draw.rect(screen, (250, 100, 100), (sheep1[0], sheep1[1], 40, 40)) #use this if you don't have a jpg
+        screen.blit(SheepPic, (sheep3[0], sheep3[1]))
+
+    if sheep4[3] == False: #don't draw them if they're already caught!
+        #pygame.draw.rect(screen, (250, 100, 100), (sheep1[0], sheep1[1], 40, 40)) #use this if you don't have a jpg
+        screen.blit(SheepPic, (sheep4[0], sheep4[1]))
+    
+    if sheep5[3] == False: #don't draw them if they're already caught!
+        #pygame.draw.rect(screen, (250, 100, 100), (sheep1[0], sheep1[1], 40, 40)) #use this if you don't have a jpg
+        screen.blit(SheepPic, (sheep5[0], sheep5[1]))
+
+
+
+
     #display score
     screen.blit(text, (20, 20))
     text2 = font.render(str(score), True, (200, 200, 0))#update score number
